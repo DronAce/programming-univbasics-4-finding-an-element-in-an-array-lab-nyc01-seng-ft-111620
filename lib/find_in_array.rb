8e009f51691b79232db12d0fde402bb056e5384f
Okechukwu O Uneze <unezeo@gmail.com>
2020-10-28 12:04:51 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
     size.times do |index|
       if array[index] == value_to_find
         return index
-      else 
+      else
         return nil
     end
 end
